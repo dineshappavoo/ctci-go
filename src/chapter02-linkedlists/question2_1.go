03ac425ba25c03f4f2bec862411ea71faa5a2162
dinesgappavoo <dinesha.cit@gmail.com>
2014-12-22 09:36:10 -0600

    Updated the folder structure

---
+++ src/chapter02-linkedlists/question2_1.go
@@ -0,0 +1,37 @@
+// Author: Dinesh Appavoo (dineshappavoo) <dinesha.cit@gmail.com>
+
+// Algorithm IS_DUPLICATE_EXIST(str):
+// 1.Get the input string
+// 2.Create a map of [string]boolean
+// 4.For i 1 to n iterate through the characters ch of the string
+//		if ch present in the map then return false
+//		else put ch to the map --> map[ch]=true
+//	5.return true
+package main
+
+import (
+	"fmt"
+)
+
+var sMap map[string]bool
+
+func main() {
+	str := "Helo World!"
+	fmt.Println(str)
+	fmt.Println(is_duplicate_present(str))
+}
+
+func is_duplicate_present(str string) bool {
+
+	sMap = make(map[string]bool)
+	var ch string
+	for i := 0; i < len(str); i++ {
+		ch = string([]rune(str)[i])
+		if sMap[ch] {
+			return false
+		} else {
+			sMap[ch] = true
+		}
+	}
+	return true
+}
