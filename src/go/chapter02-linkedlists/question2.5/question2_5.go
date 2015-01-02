e1c1eb4de5357550c3afb13b9e65ad5b87e86dbe
dinesgappavoo <dinesha.cit@gmail.com>
2015-01-02 04:50:41 -0500

    Updated q 2.5

--- src/go/chapter02-linkedlists/question2.5/question2_5.go
+++ src/go/chapter02-linkedlists/question2.5/question2_5.go
@@ -4,7 +4,8 @@
  Algorithm ADD_LISTS(list1,list2):
  1.Get the input list1 and list2
  2.if list1==nil && list2==nil then return nil
- 	else  return list1==nil?list2:list1
+ 	else  if list == nill || list2 == nil
+ 		return list1==nil?list2:list1
  3.add elements and maintain carry from lists until both the lists has elements
  4.Add carry to the continuing list and add to result list if the lists are of different size
  5.return the result list
@@ -23,33 +24,65 @@ func main() {
 	l.PushFront(4)
 	l.PushFront(5)
 	l.PushFront(7)
-	l.PushFront(6)
-	l.PushBack(9)
-	res := splitAroundX(l, 6)
+	l.PushFront(9)
+
+
+	m := list.New()
+	m.PushFront(3)
+	m.PushFront(2)
+	m.PushFront(8)
+	//m.PushFront(6)
+	res := addLists(l, m)
 	for e := res.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value)
+		fmt.Print(e.Value)
 	}
+	fmt.Println(" ")
+
 
 }
 
 //ERROR
 
-func splitAroundX(l *list.List, x int) *list.List {
+func addLists(l *list.List, m *list.List) *list.List {
 
-	if l == nil {
+	if l == nil && m == nil {
 		return nil
 	}
-	lThanX := list.New()
-	gThanX := list.New()
+	lLength := l.Len()
+	mLength := m.Len()
 
-	for e := l.Front(); e != nil; e = e.Next() {
-		m := e.Value.(int)
-		if m < x {
-			lThanX.PushBack(m)
-		} else {
-			gThanX.PushBack(m)
-		}
+	carry := 0
+	value := 0
+	resList := list.New()
+
+	var e *list.Element
+	var f *list.Element
+	for e, f = l.Front(), m.Front(); e != nil && f != nil; e, f = e.Next(), f.Next() {
+		value = carry + e.Value.(int) + f.Value.(int)
+		carry = 0
+		carry = value / 10
+		value = value % 10
+		resList.PushFront(value)
 	}
-	lThanX.PushBackList(gThanX)
-	return lThanX
+
+	var p *list.Element
+	if lLength > mLength {
+		p = e
+	} else {
+		p = f
+	}
+
+	for ; p != nil; p = p.Next() {
+		value = carry + p.Value.(int)
+		carry = 0
+		carry = value / 10
+		value = value % 10
+		resList.PushFront(value)
+	}
+
+	if carry != 0 {
+		resList.PushFront(carry)
+	}
+
+	return resList
 }
