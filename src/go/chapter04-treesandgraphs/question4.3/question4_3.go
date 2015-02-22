0b30d5a179f6b39456097c7005eaeb130f1d71cd
dinesgappavoo <dinesha.cit@gmail.com>
2015-02-21 18:14:21 -0600

    Updated question 4.3

--- src/go/chapter04-treesandgraphs/question4.3/question4_3.go
+++ src/go/chapter04-treesandgraphs/question4.3/question4_3.go
@@ -1,51 +1,35 @@
 // Author: Dinesh Appavoo (dineshappavoo) <dinesha.cit@gmail.com>
 
 /*Algorithm BST_MINIMAL_HEIGHT(arr, low, high): 
- 1.Get the input array arr
- 2.find the mid element using low and high . mid =(low+high)/2
- 3.t.Value = arr[mid]
- 4.t.Left=BST_MINIMAL_HEIGHT(arr, low, mid)
- 5.t.Right=BST_MINIMAL_HEIGHT(arr, mid+1, high)
- 6.return t
+1.Get the input array arr
+2.find the mid element using low and high . mid =(low+high)/2
+3.t.Value = arr[mid]
+4.t.Left=BST_MINIMAL_HEIGHT(arr, low, mid)
+5.t.Right=BST_MINIMAL_HEIGHT(arr, mid+1, high)
+6.return t
 */
 package main
 
 import (
 	"fmt"
 	"go/chapter04-treesandgraphs/binarytree"
-	"math"
 )
 
 func main() {
 
-	t1 := binarytree.New(100, 1)
-	height := getHeight(t1)
-	fmt.Println(height)
-
-	fmt.Println("Tree Balanced is ", isBalanced(t1))
+	inArr := []int{4, 5, 7, 8, 9}
+	t1 := getMinimalBST(inArr, 0, len(inArr)-1)
+	fmt.Println("Output", t1)
 }
 
 func getMinimalBST(arr []int, low int, high int) *binarytree.Tree {
-if low>high {
-	return nil
-}
-
-}
-
-func getHeight(t *binarytree.Tree) float64 {
-	if t == nil {
-		return 0
+	if low > high {
+		return nil
 	}
-	return math.Max(getHeight(t.Left), getHeight(t.Right)) + 1
-}
-
-func isBalanced(t *binarytree.Tree) bool {
-	if t.Left == nil && t.Right == nil {
-		return true
-	}
-	heightDiff := math.Abs(getHeight(t.Left) - getHeight(t.Right))
-	if heightDiff > 1 {
-		return false
-	}
-	return isBalanced(t.Left) && isBalanced(t.Right)
+	mid := (low + high) / 2
+	t1 := binarytree.NewTree()
+	t1.Value = arr[mid]
+	t1.Left = getMinimalBST(arr, low, mid)
+	t1.Right = getMinimalBST(arr, mid+1, high)
+	return t1
 }
