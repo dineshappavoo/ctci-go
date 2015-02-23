78880b44d9112cdfc15b9e4d84710b6fb6cbaf6d
dinesgappavoo <dinesha.cit@gmail.com>
2015-02-23 11:08:06 -0600

    Updated 4.2

--- src/go/chapter04-treesandgraphs/question4.2/question4_2.go
+++ src/go/chapter04-treesandgraphs/question4.2/question4_2.go
@@ -1,12 +1,21 @@
 // Author: Dinesh Appavoo (dineshappavoo) <dinesha.cit@gmail.com>
 
-/*Algorithm BST_MINIMAL_HEIGHT(arr, low, high): 
-1.Get the input array arr
-2.find the mid element using low and high . mid =(low+high)/2
-3.t.Value = arr[mid]
-4.t.Left=BST_MINIMAL_HEIGHT(arr, low, mid)
-5.t.Right=BST_MINIMAL_HEIGHT(arr, mid+1, high)
-6.return t
+/*Algorithm IS_PATH_AVAILABLE(t1, t2):
+1.Get he input nodes t1, t2
+2.Create a queue q <- Empty
+3.if t1.Visited==false
+	q.Push(t1)
+	t1.Visited=true
+4.while(!q.IsEmpty())
+	node=q.Poll()
+	adjacents = node.Adjacents
+	for n in adjacents
+		if n.Visited == false
+			if n.Value = t2.Value
+				return true
+			q.Push(n)
+			n.Visited=true
+5.return false
 */
 package main
 
