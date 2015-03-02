e58255222c546c6a29364f3fde3c439aa39a59b3
dinesgappavoo <dinesha.cit@gmail.com>
2015-03-02 02:31:10 -0600

    dded fibonacci

---
+++ src/go/chapter09/fibonacci.go
@@ -0,0 +1,42 @@
+// Author: Dinesh Appavoo (dineshappavoo) <dinesha.cit@gmail.com>
+
+/*
+Algorithm IS_DUPLICATE_EXIST(str):
+ 1.Get the input string
+ 2.Create a map of [string]boolean
+ 4.For i from 1 to n iterate through the characters ch of the string
+		if ch present in the map then return false
+		else put ch to the map --> map[ch]=true
+	5.return true
+*/
+package main
+
+import (
+	"fmt"
+)
+
+var sMap memoTable[int]int
+
+func main() {
+	n := 8
+	fibVal := findFibonacii(n)
+	fmt.Println("Fibonacii value of %d is ",n, fibVal)
+}
+
+func findFibonacii(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+
+	if val, ok := memoTable[n]; ok {
+		return memoTable[n]
+	}
+	fibVal := findFibonacii(n-1) + findFibonacii(n-2)
+	memoTable[n] = fibVal
+	return memoTable[n]
+
+}
+
